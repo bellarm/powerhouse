1f20191074253d6a8c1b46ade11bbbfa3679d521
Bella Mangunsong <rachelitabella@rocketmail.com>
2018-07-26 21:05:32 +1000

    pls work

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from '@/components/Home'
-//import About from '@/components/About'
+import About from '@/components/About'
 
 Vue.use(Router)
 
@@ -13,13 +13,13 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
     }
   ]
 })
 /*
-,
-{
-  path: '/about',
-  name: 'About',
-  component: About
-}*/
+*/
